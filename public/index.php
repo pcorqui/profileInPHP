5f48b3e81813949939fac4f64718f31794031406
chacalonchacaloso <pcorqui@gmail.com>
2020-07-25 15:39:08 -0500

    haber si despliega

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-ini_set('display_errors',1);
+/*ini_set('display_errors',1);
 ini_set('display_starup_error',1);
 
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 require_once '../vendor/autoload.php';
 
